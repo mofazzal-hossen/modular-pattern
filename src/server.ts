225a01d7a0d2fdbeac525c921c1dee20fdb6eddd
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-07 01:03:09 +0600

    feat: implement CRUD operations for user management in API

--- src/server.ts
+++ src/server.ts
@@ -109,7 +109,116 @@ res.status(500).json({
 })
 
   }
-})
+});
+
+
+app.get('/api/user:id', async(req:Request, res:Response)=>{
+    const {id}= req.body
+try {
+  const result = await pool.query(`
+     SELECT * FROM users WHERE id=$1  
+    `,[id])
+        if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+        data: {},
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User retrived successfully!",
+      data: result.rows[0],
+    });
+
+} catch (error: any) {
+  res.status(500).json({
+    success: false,
+      message: error.message,
+      error: error,
+  })
+
+}
+}); 
+
+
+app.put("/api/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, password, age, is_active } = req.body;
+
+  // console.log("Id : ", id);
+  // console.log({ name, password, age, is_active });
+
+  try {
+    const result = await pool.query(
+      `
+    UPDATE users 
+    SET 
+    name=COALESCE($1,name),
+    password=COALESCE($2,password),
+    age=COALESCE($3,age),
+    is_active=COALESCE($4,is_active) 
+
+    WHERE id=$5 RETURNING *
+    `,
+      [name, password, age, is_active, id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+      });
+    }
+
+    // console.log(result);
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+app.delete("/api/user/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+    DELETE FROM users WHERE id=$1  
+      `,
+      [id],
+    );
+
+    console.log(result);
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully!",
+      data: {},
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
 
 
 
