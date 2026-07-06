5b3ccb603e21fab746e74b63ac90415472261230
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-06 22:44:03 +0600

    feat: add PostgreSQL dependency and connection setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { type Application, type Request, type Response } from 'express'
-
+import {Pool} from "pg"
 const app : Application = express()
 const port = 9000
 // you must used all time MeddleWare 
@@ -7,6 +7,14 @@ app.use(express.json())
 app.use(express.text()) //show text data 
 app.use(express.urlencoded({extended:true})) //{extended:true} all type data show as like nested-data. 
 //end
+
+//pg connection 
+
+const pool = new Pool({
+  connectionString:"postgresql://neondb_owner:npg_hS6D7qkWBouK@ep-calm-leaf-atx7sci5-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+})
+
 
 app.get('/', (req :Request, res:Response) => {
   // res.send('Hello World')
