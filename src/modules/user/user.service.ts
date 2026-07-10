ae96947fdd380221420e7a5e3182198ef6f7de7a
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-11 00:47:36 +0600

    feat: add bcryptjs for password hashing in user creation and update password field type to TEXT

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,18 +1,29 @@
 import { pool } from "../../db";
 import type { IUser } from "./user.interface";
+import bcrypt from "bcryptjs";
+
 
 const createUserIntoDB = async(payload: IUser) =>{
 
 const {name, email, password, age} = payload
+//password hash
+const hashPassword = await bcrypt.hash(password, 10)
 
  const result = await pool.query(`
     INSERT INTO users (name, email,  password ,age  )  
     VALUES ($1,$2,$3,$4) RETURNING *
     
-    `, [name, email, password, age]);
-
+    `, 
+    [name, email, hashPassword, age],
+  );
+///obj in to delete password mahout
+delete result.rows[0].password;
     return result;
 };
+///  VALUES ($1,$2,$3,$4) RETURNING * . {* = all data res }
+
+
+
 
 const getAllUserFromDB = async()=>{
      const result = await pool.query(`
