06428cd0de08b96802dbf7f6d831fc52df0c8b9e
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-13 01:57:06 +0600

    feat: enhance authentication middleware with JWT verification and user validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
+import  jwt, { type JwtPayload }  from 'jsonwebtoken';
+import config from "../config";
+import { pool } from "../db";
+
+
 
 
 
@@ -12,9 +17,33 @@ const auth = ()=>{
         res.status(401).json({
             success:"false",
             message:"Unauthorized access!!!"
-        })
-        
-    }
+        })   
+    };
+
+
+    const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload;
+    // console.log(decoded)
+ const userData = await pool.query(`SELECT * FROM users WHERE email = $1`, [decoded.email],)
+//  console.log(userData)
+const user = userData.rows[0];
+// console.log(user)
+
+if (userData.rows.length === 0) {
+    res.status(404).json({
+        success:"false",
+        message:"user is not found"
+    })
+};
+
+if (!user.is_active) {
+    res.status(404).json({
+        success:"false",
+        message:"forbidden!!"
+    })
+};
+
+
+
   next();  
 };
 }
