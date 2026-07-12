7f1216df716978b2d979456ed1ee5bbc087e988b
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-13 00:36:28 +0600

    feat: add logging middleware to track API requests in logger.txt

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
-import express, { type Application, type Request, type Response } from 'express'
-import config from './config'
-import { pool } from './db'
+import express, { type Application, type NextFunction, type Request, type Response } from 'express'
+
+import fs from "fs";
+import path from "path";
 import { userRoute } from './modules/user/user.route'
 import { profileRoute } from './modules/profile/profile.route'
 import { authRoute } from './modules/auth/auth.route'
+import { error } from 'console';
+
 const app: Application = express()
 
 // you must used all time MeddleWare 
 app.use(express.json())
 app.use(express.text()) //show text data 
 app.use(express.urlencoded({ extended: true })) //{extended:true} all type data show as like nested-data. 
-//end
 
+app.use((req, res, next) => {
+  const log = `[${new Date().toISOString()}] ${req.method} ${req.url}\n`;
+
+  fs.appendFile("logger.txt", log, (err) => {
+    if (err) {
+      console.error("Failed to write log:", err);
+    } else {
+      console.log("Log saved successfully");
+    }
+  });
+
+  next();
+});
 
 
 
