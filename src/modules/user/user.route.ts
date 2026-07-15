4fef216dfc1a5976365ddddf1d57b6c8ca8ccf9c
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-15 17:43:26 +0600

    feat: define USER_ROLE constants and update user route authentication to use them

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,13 +1,14 @@
 import { Router, } from "express";
 import { userController } from "./user.controller";
 import auth from "../../middleware/auth";
+import { USER_ROLE } from "../../types";
 
 
 const router = Router()
 
 
 router.post('/', userController.createUser);
-router.get('/',auth("admin","agent"), userController.getAllUser);
+router.get('/',auth(USER_ROLE.admin, USER_ROLE.agent), userController.getAllUser);
 router.get('/:id', userController.getSingleUser); 
 router.put("/:id", userController.updateUserID);
 router.delete("/:id", userController.userDelete);
